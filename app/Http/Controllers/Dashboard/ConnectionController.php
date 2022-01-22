48d7c18763057fafdbd87f487117b841012364b4
VladimirArtsruni <vladimir.artsruni@gmail.com>
2022-01-22 22:59:07 +0400

    fix

--- app/Http/Controllers/Dashboard/ConnectionController.php
+++ app/Http/Controllers/Dashboard/ConnectionController.php
@@ -353,6 +353,7 @@ class ConnectionController extends Controller
         $connection = $this->connectionRepository->getById($id);
         GetConnectionPositions::dispatch($account, $connection);
         GetConnectionSkills::dispatch($account, $connection);
+        GetConnectionCareerInterest::dispatch($account, $connection);
         $this->putFlashMessage(true, 'Successfully run job');
         return redirect()->back();
     }
